87f3cce425be1d5768259dbee46b5b30a8a08681
chidu25 <128888672+chidu25@users.noreply.github.com>
2025-11-19 22:46:05 +0530

    Update index.js

--- client/index.js
+++ client/index.js
@@ -3,7 +3,4 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 
-// ❌ REMOVE THIS BAD IMPORT
-// import routes from "./routes.js";
-
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
